b5602a0c74c4d44b4608c4e4c1e21de585b8e00e
Hakeem-7 <52264207+Hakeem-7@users.noreply.github.com>
2019-10-04 09:18:33 -0500

    Online repository

---
+++ hw_2.R
@@ -0,0 +1,34 @@
+# Q (1b)
+set.seed(7)
+uni = function(x){dunif(x,0,10)}
+x <- seq(-5,15,0.001)
+x
+plot(x, uni(x), typ = 'b', col = 'dark red')
+
+# Q.1b
+
+cdf = function(x){punif(x,0,10)}
+plot(x,cdf(x),type='l', col = 'dark green', lwd = 3)
+     
+
+# Q.1c (i)
+
+m = runif(50,0,10)
+hist(m, probability = T, breaks = 20)
+curve(dunif(x,0,10), col= 2, lwd=2, add=TRUE)
+
+# Q.1c (ii)
+plot(ecdf(m))
+curve(punif(x,0,10), col= 2, lwd=2, add=TRUE)
+
+
+# Q.1d 
+j = runif(1000,0,10)
+hist(j, probability = T, breaks = 15)
+curve(dunif(x,0,10), col= 'blue', lwd=2, add=TRUE)
+
+# Q.1d (ii)
+plot(ecdf(j))
+curve(punif(x,0,10), col= 2, lwd=2, add=TRUE)
+
+
